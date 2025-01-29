261fb41371653e34a419a0e2086765d1045e8d76
Paripelli Jayakrishna <krishna9502202491@gmail.com>
2025-01-30 01:08:37 +0530

    Update Help.jsx

--- src/routes/help/Help.jsx
+++ src/routes/help/Help.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Navbar from "/src/components-Common/Navbar";
+import Navbar from "/src/Components-Common/Navbar";
 import Chatbot from "/src/routes/help/components/Chatbot";
 
 const Help = () => {
