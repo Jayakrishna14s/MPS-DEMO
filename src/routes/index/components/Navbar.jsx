eea5fdf3f3f2ec6b6661fdd609a17200d0086965
Paripelli Jayakrishna <krishna9502202491@gmail.com>
2025-02-05 20:57:45 +0530

    Update Navbar.jsx

--- src/routes/index/components/Navbar.jsx
+++ src/routes/index/components/Navbar.jsx
@@ -83,7 +83,7 @@ const Navbar = () => {
           }`}
           id="navbar-cta"
         >
-          <ul className="bg-white text-[#035c67] flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg  md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+          <ul className="bg-white text-[#035c67] flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg  md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0   dark:border-gray-700">
             <li>
               <a
                 // href="#about"
@@ -99,7 +99,7 @@ const Navbar = () => {
               <a
                 // href="#services"
                 onClick={() => handleScroll("services")}
-                className={`cursor-pointer block py-2 px-3 md:p-0 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:scale-105 md:dark:hover:scale-105 t dark:border-gray-700 ${
+                className={`cursor-pointer block py-2 px-3 md:p-0 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:scale-105 md:dark:hover:scale-105  dark:border-gray-700 ${
                   activeSection === "services" && "underline text-[#3EFFC0]"
                 }`}
               >
